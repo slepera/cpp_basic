d72fadfa9d8f866a97ea9472ee42352c61f152ba
Salvatore <salvatore.lepera.lavoro@gmail.com>
2024-12-27 20:58:50 +0100

    added ignore file

--- naval_battle.cpp
+++ naval_battle.cpp
@@ -4,46 +4,85 @@ using namespace std;
 
 const int DIM = 10;
 const int NSHIPS = 5;
+int hit = 0;
 
 
 
-void print(bool field[DIM][DIM])
+void print(unsigned short int field[DIM][DIM])
 {
-    cout<<"  A  B  C  D  E  F  G  H  I  L"<<endl;
+    cout<<"      A    B    C    D    E    F    G    H    I    L"<<endl;
     for(int i = 0; i < DIM; i++)
     {
-        cout<<i+1;
+        cout<<i+1<<"   ";
         for(int j = 0; j < DIM; j++)
         {
-            if(field[i][j]==true)
+            if(field[i][j]==0)
             {
-                cout<<" * ";
+                cout<<"     ";
+            }
+            else if(field[i][j]==1)
+            {
+                cout<<"  *  ";
             }
             else
             {
-                cout<<"   ";
+                cout<<"  x  ";
             }
         }
-        cout<<endl;
+        cout<<endl<<endl;
     }
     cout<<endl<<endl<<endl;
 }
 
-void generate_fields(bool field[DIM][DIM])
+void generate_fields(unsigned short int field[DIM][DIM])
 {
     for(int i = 0; i < NSHIPS; i++)
     {
         int x = rand()%DIM;
         int y = rand()%DIM;
-        field[x][y] = true;
+        field[x][y] = 1;
+        cout<<x<<" "<<y<<" ";
     }
+    cout<<endl;
 }
 
-
+void shoot(unsigned short int field[DIM][DIM])
+{
+    string xs;
+    int x, y;
+    cout<<"inserisci la coordinata x (A-Z):";
+    cin>>xs;
+    cout<<"inserisci la coordinata y (1-10):";
+    cin>>y;
+    x = tolower(xs[0])-97;
+    y = y-1;
+    cout<<x<<" "<<y<<" ";
+    if(field[x][y]==1)
+    {
+        field[x][y]=2;
+        hit++;
+    }
+    
+}
+bool is_over()
+{
+    if(hit == NSHIPS)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 int main()    
 {
-    bool field_1[DIM][DIM];
-    bool field_2[DIM][DIM];
+    unsigned short int field_1[DIM][DIM];
+    unsigned short int field_2[DIM][DIM];
     generate_fields(field_1);
     print(field_1);
+    do{
+        shoot(field_1);
+        print(field_1);
+    }while(!is_over());
+    cout<<"game over!";
 }
