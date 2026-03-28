08353856dee6d2d651a91c61ee1f2144b0bd3ded
Salvatore <salvatore.lepera.lavoro@gmail.com>
2026-03-28 11:40:14 +0100

    migliorie varie

--- tris.cpp
+++ tris.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
+
 const int R=3;
 const int C=3;
+const int CERCHIO = 1;
+const int CROCE = 2;
+const int NON_FINITA = 3;
+const int PARI = 4;
 void reset_m(char matrice[][C]) {
     for (int i=0; i<R; i++) {
         for (int j=0; j<C; j++) {
@@ -10,31 +15,51 @@ void reset_m(char matrice[][C]) {
     }
 }
 void stampa (char matrice[][C]) {
+    cout<<"-------------------------"<<endl;
     for (int i=0; i<R; i++) {
         for (int j=0; j<C; j++) {
-            cout<<matrice[i][j]<<"\t";
+            cout<<"|"<<matrice[i][j]<<"\t";
         }
-        cout<<endl;
+        cout<<"|"<<endl<<"-------------------------"<<endl;
     }
 }
-bool partita_finita (char matrice[][C]) {
+int partita_finita (char matrice[][C]) {
     for (int i=0; i<R; i++) {
-        if (matrice[i][0]!=' ' && matrice[i][0] == matrice[i][1] && matrice [i][1] == matrice[i][2] ) {
-            return true;
+        if (matrice[i][0]=='o' && matrice[i][0] == matrice[i][1] && matrice [i][1] == matrice[i][2] ) {
+            return CERCHIO;
+        }
+        if (matrice[i][0]=='x' && matrice[i][0] == matrice[i][1] && matrice [i][1] == matrice[i][2] ) {
+            return CROCE;
         }
     }
     for (int j=0; j<C; j++) {
-        if (matrice[0][j]!=' ' && matrice[0][j] == matrice[1][j] && matrice [1][j] == matrice[2][j] ) {
-            return true;
+        if (matrice[0][j]=='o' && matrice[0][j] == matrice[1][j] && matrice [1][j] == matrice[2][j] ) {
+            return CERCHIO;
+        }
+        if (matrice[0][j]=='x' && matrice[0][j] == matrice[1][j] && matrice [1][j] == matrice[2][j] ) {
+            return CROCE;
         }
     }
-    if (matrice[0][0]!=' ' && matrice[0][0] == matrice[1][1] && matrice[1][1] == matrice[2][2]) {
-        return true;
+    if (matrice[0][0]=='o' && matrice[0][0] == matrice[1][1] && matrice[1][1] == matrice[2][2]) {
+        return CERCHIO;
     }
-    if (matrice[2][0]!=' ' && matrice[2][0] == matrice[1][1] && matrice[1][1] == matrice[0][2]) {
-        return true;
+    if (matrice[0][0]=='x' && matrice[0][0] == matrice[1][1] && matrice[1][1] == matrice[2][2]) {
+        return CROCE;
     }
-    return false;
+    if (matrice[2][0]=='o' && matrice[2][0] == matrice[1][1] && matrice[1][1] == matrice[0][2]) {
+        return CERCHIO;
+    }
+    if (matrice[2][0]=='x' && matrice[2][0] == matrice[1][1] && matrice[1][1] == matrice[0][2]) {
+        return CROCE;
+    }
+    for (int i=0; i<R; i++) {
+        for (int j=0; j<C; j++) {
+            if (matrice[i][j]==' ') {
+                return NON_FINITA;
+            }
+        }
+    }
+    return PARI;
 }
 void turno(char matrice[][C], bool cerchio) {
     int riga, colonna;
@@ -43,24 +68,37 @@ void turno(char matrice[][C], bool cerchio) {
     cout<<"inserisci la colonna: ";
     cin>>colonna;
     if (cerchio) {
-        matrice[riga][colonna]='o';
+        matrice[riga-1][colonna-1]='o';
     } else {
-        matrice[riga][colonna]='x';
+        matrice[riga-1][colonna-1]='x';
     }
 }
 int main() {
+    int risultato;
     char matrice[R][C];
     bool cerchio=true;
     reset_m(matrice);
     do {
+        system("cls");
+        if (cerchio) {
+            cout<<"Tocca a cerchio!!!"<<endl;
+        }else {
+            cout<<"Tocca a croce!!!"<<endl;
+        }
         stampa(matrice);
         turno(matrice, cerchio);
         cout<<endl;
         cerchio=!cerchio;
-    } while (!partita_finita(matrice));
-
-
-
-
+        risultato = partita_finita(matrice);
+    } while (risultato==NON_FINITA);
+    system("cls");
+    stampa(matrice);
+    if (risultato==CERCHIO) {
+        cout<<"Ha vinto cerchio!!!";
+    }else if (risultato==CROCE) {
+        cout<<"Ha vinto croce!!!";
+    }else if (risultato==PARI) {
+        cout<<"Parita'!!!";
+    }
     return 0;
 }
